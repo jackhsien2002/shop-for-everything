c2578a47d6dcf278bf85c71e2cf765acfd92ef5b
jackhsien2002 <jackhsien2002@gmail.com>
2020-01-31 11:09:58 +0800

    style product create, change product form

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from .models import Product
 from django.forms import ModelForm, HiddenInput
+from django.utils.translation import gettext_lazy as _
 
 class ProductForm(ModelForm):
 
@@ -7,3 +8,11 @@ class ProductForm(ModelForm):
         model = Product
         fields = ['author', 'name', 'description', 'price', 'stock', 'image']
         widgets = {'author':HiddenInput()}
+        labels = {
+            'author': _('店家'),
+            'name': _('商品名稱'),
+            'description': _('商品描述'),
+            'price': _('價位'),
+            'stock': _('庫存'),
+            'image': _('商品圖片'),
+        }
